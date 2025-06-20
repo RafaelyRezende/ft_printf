2afa54426a39791975a0e3f67c56c5f424c413d6
RafaelyRezende <rafael.yanase@gmail.com>
2025-06-20 14:57:01 +0000

    Added logic for flags

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,16 +6,18 @@
 /*   By: rluis-ya <rluis-ya@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 13:46:21 by rluis-ya          #+#    #+#             */
-/*   Updated: 2025/06/20 11:02:59 by rluis-ya         ###   ########.fr       */
+/*   Updated: 2025/06/20 14:56:33 by rluis-ya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_format_specifier(va_list args, char *c)
+void	ft_decimal(va_list args, uint8_t flag)
 {
-	while (*(++c))
-	{
+	
+}
+uint8_t	ft_format_specifier(va_list args, char *c)
+{
 		if (*c == 'd' || *c == 'i')
 			return (ft_decimal(va_arg(args, int)));
 		else if (*c == 'c')
@@ -29,9 +31,41 @@ void	ft_format_specifier(va_list args, char *c)
 		else if (*c == 'x' || *c == 'X')
 			return (ft_hexa(va_arg(args, unsigned int)));
 		else
+		{
 			putchar(*c);
+			return (1);
+		}
+}
+
+uint8_t	ft_check_flags(char **c)
+{
+	uint8_t	flag;
+
+	flag = 0;
+	while (ft_is_flag(**c))
+	{
+		if (**c == '-')
+			flag |= MINUS;
+		if (**c == '0')
+			flag |= ZERO;
+		if (**c == '.')
+			flag |= DOT;
+		if (**c == '#')
+			flag |= POUND;
+		if (**c == ' ')
+			flag |= SPACE;
+		if (**c == '+')
+			flag |= PLUS;
+		(*c)++;
 	}
-	return (1);
+	return (flag);
+}
+
+int	ft_is_flag(char c)
+{
+	if (c == '-' || c == '0' || c == '.' || c == '#' || c == ' ' || c == '+') 
+		return (1);
+	return (0);
 }
 
 void	ft_putchar(const char c)
@@ -43,18 +77,25 @@ int	ft_printf(const char *fmt, ...)
 {
 	va_list 		args;
 	uint8_t			flags;
-	uint8_t			state;
+	int				state;
 
+	state = 0;
 	va_start(args, fmt)
 	while (fmt && *fmt)
 	{
 		if (*fmt != '%')
+		{
 			ft_putchar(fmt);
+			state++;
+		}
 		else
-			ft_format_specifier(args, fmt);
+		{
+			flags = ft_check_flags(fmt);
+			state += ft_format_specifier(args, fmt);
+		}
 		fmt++;
 	}
-	return (0);
+	return (state);
 }
 
 int	main(void)
